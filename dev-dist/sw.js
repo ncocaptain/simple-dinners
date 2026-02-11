9c215694ba78447eed1936a8fb8f97d4db74735b
ncocaptain <redskintom88@gmail.com>
2026-02-11 17:59:50 -0500

    Background + cookbook fixes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5anlujt1bi4"
+    "revision": "0.68je0hfrkno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
